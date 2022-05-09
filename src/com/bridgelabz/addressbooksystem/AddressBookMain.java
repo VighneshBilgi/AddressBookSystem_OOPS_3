c2d82adc05da48af83d1560846da0e68208466a7
VighneshBilgi <vhbilgi@gmail.com>
2022-05-09 18:15:25 +0530

    Added: UC1_Contacts

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -2,6 +2,6 @@ package com.bridgelabz.addressbooksystem;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book Program");
+        System.out.println("Welcome to Address Book Program");
     }
 }
